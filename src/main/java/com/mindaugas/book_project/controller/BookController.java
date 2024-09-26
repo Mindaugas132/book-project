c8774c0af20250c4fa4c868465615903d2a9aceb
Mindaugas132 <mindaugas132@outlook.com>
2024-09-26 11:52:05 +0300

    Add BookController

---
+++ src/main/java/com/mindaugas/book_project/controller/BookController.java
@@ -0,0 +1,62 @@
+package com.mindaugas.book_project.controller;
+
+import com.mindaugas.book_project.entity.Book;
+import com.mindaugas.book_project.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+
+    @Autowired
+    private BookService bookService;
+
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return bookService.getAllBooks();
+    }
+
+    @GetMapping("/filter")
+    public List<Book> filterBooks(@RequestParam(required = false) String title,
+                                  @RequestParam(required = false) String author,
+                                  @RequestParam(required = false) Integer year,
+                                  @RequestParam(required = false) String genre,
+                                  @RequestParam(required = false) Double rating) {
+        if (title != null) {
+            return bookService.getBooksByTitle(title);
+        } else if (author != null) {
+            return bookService.getBooksByAuthor(author);
+        } else if (year != null) {
+            return bookService.getBooksByYear(year);
+        } else if (genre != null) {
+            return bookService.getBooksByGenre(genre);
+        } else if (rating != null) {
+            return bookService.getBooksByRating(rating);
+        } else {
+            return bookService.getAllBooks();
+        }
+    }
+
+    @PostMapping
+    public Book addBook(@RequestBody Book book) {
+        return bookService.saveBook(book);
+    }
+
+    @PutMapping("/{id}/rate")
+    public Book rateBook(@PathVariable Long id, @RequestParam double rating) {
+
+        Optional<Book> optionalBook = bookService.findById(id);
+
+        if (optionalBook.isPresent()) {
+            Book book = optionalBook.get();
+            book.setRating(rating);
+            return bookService.saveBook(book);
+        } else {
+            throw new IllegalArgumentException("Book not found with id: " + id);
+        }
+    }
+}
